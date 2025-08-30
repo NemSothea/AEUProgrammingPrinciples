3fe509b9f28081f75730f832377151c79812fb9f
sothea007 <sothea007@sothea007s-MacBook-Pro.local>
2025-08-30 16:01:16 +0700

    Complete lab activity

--- src/main/java/com/example/demo/grademanagementsystem/StudentGradeManagementSystem.java
+++ src/main/java/com/example/demo/grademanagementsystem/StudentGradeManagementSystem.java
@@ -1,16 +1,26 @@
 package com.example.demo.grademanagementsystem;
+/*
+ * 
+ * Create a Student Grade Management System
+Build a robust student grade management system
+- Read student grades from csv file (student.csv)
+- Calculate average grade for each student
+- write results to output file(result.txt)
+- handle all possible exceptions gracefully
+- Create log file for all operations
 
-
+*/
 public class StudentGradeManagementSystem {
-     private static final String CSV_FOLDER = "src/main/java/com/example/demo/grademanagementsystem/";
+
+    private static final String CSV_FOLDER = "src/main/java/com/example/demo/grademanagementsystem/";
     private static final String CSV_FILE = CSV_FOLDER + "students.csv";
 
     public static void main(String[] args) {
         CustomLogger logger = CustomLogger.getInstance();
-        
+
         try {
             logger.logInfo("Starting student grade management system");
-            
+
             GradeManagementSystem system = new GradeManagementSystem();
 
             system.clearStudents();
@@ -34,5 +44,5 @@ public class StudentGradeManagementSystem {
             e.printStackTrace();
         }
     }
-    
+
 }
